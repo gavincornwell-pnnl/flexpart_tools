a0327900390f0af2226bf606d26a4214e0ebbc5e
Gavin Cornwell <gavinccornwell@gmail.com>
2026-04-08 10:37:52 -0700

    fix flexwrf input file location

--- flexwrf_write.py
+++ flexwrf_write.py
@@ -1,4 +1,4 @@
-def writeFLEXWRF_input(StartTime,StopTime,NumPart,Longitude,Latitude,OutputPath,MetPath='',Available='',TemplateFile='./input.hrrr.template',NewInputFile):
+def writeFLEXWRF_input(StartTime,EndTime,NumPart,Longitude,Latitude,NewInputFile,OutputPath,MetPath,Available,TemplateFile='./input.hrrr.template'):
     '''
     Function to write a flexpart-wrf file using a template file. The key parameters, are:
     StartTime: as python datetiem
@@ -16,29 +16,35 @@ def writeFLEXWRF_input(StartTime,StopTime,NumPart,Longitude,Latitude,OutputPath,
     ### read in file
     fid = open(TemplateFile)
     lines = fid.readlines()
+#    print(len(lines))
     fid.close()
     
     ### write paths
+    if OutputPath[-1] != '/':
+        OutputPath = OutputPath+'/'
+    if MetPath[-1] != '/':
+        MetPath = MetPath+'/'
     lines[1] = OutputPath + '\n'
     lines[2] = MetPath + '\n'
     lines[3] = Available + '\n'
 
     ### write start and end time
     StartTimeFix = StartTime - dt.timedelta(days=5) # go back five days
-    lines[7] = '   %s    YYYYMMDD HHMISS   starting date of simulation\n' % StartTimeFix.strftime('%Y%m%d %H%M%S')
-    lines[8] = '   %s    YYYYMMDD HHMISS   end date of simulation\n' % EndTime.strftime('%Y%m%d %H%M%S')
+    lines[7] = '    %s    YYYYMMDD HHMISS   starting date of simulation\n' % StartTimeFix.strftime('%Y%m%d %H%M%S')
+    lines[8] = '    %s    YYYYMMDD HHMISS   end date of simulation\n' % EndTime.strftime('%Y%m%d %H%M%S')
     
     #### release information
-    lines[93] = '   %s   YYYYMMDD HHMISS   beginning date of simulation\n' % StartTime.strftime('%Y%m%d %H%M%S')
-    lines[94] = '   %s   YYYYMMDD HHMISS   ending date of simulation\n' % EndTime.strftime('   %Y%m%d %H%M%S')
-    lines[95] = '   %s        XPOINT1 (real)  longitude [deg] of lower left corner\n' % str(Longitude)
-    lines[96] = '   %s        XPOINT1 (real)  latitude [deg] of lower left corner\n' % str(Latitude)
-    lines[97] = '   %s        XPOINT2 (real)  longitude [deg] of upper right corner\n' % str(Longitude)
-    lines[98] = '   %s        YPOINT2 (real)  latitude [deg] of upper right corner\n' % str(Longitude)
-    lines[103] = '  %s           NPART (int)    total number of particles to be released' 
-    lines[104] = '   %s       NAME OF RELEASE LOCATION^M\n' % 'hrrr' + 
+    lines[76] = '   %s   YYYYMMDD HHMISS   beginning date of simulation\n' % StartTime.strftime('%Y%m%d %H%M%S')
+    lines[77] = '   %s   YYYYMMDD HHMISS   ending date of simulation\n' % EndTime.strftime('%Y%m%d %H%M%S')
+    lines[78] = '   %s        XPOINT1 (real)  longitude [deg] of lower left corner\n' % str(Longitude)
+    lines[79] = '   %s        XPOINT1 (real)  latitude [deg] of lower left corner\n' % str(Latitude)
+    lines[80] = '   %s        XPOINT2 (real)  longitude [deg] of upper right corner\n' % str(Longitude)
+    lines[81] = '   %s        YPOINT2 (real)  latitude [deg] of upper right corner\n' % str(Latitude)
+    lines[85] = '  %s           NPART (int)    total number of particles to be released\n' % str(NumPart) 
+    simname = os.path.basename(NewInputFile)
+    lines[87] = '   %s       NAME OF RELEASE LOCATION^M\n' % simname 
 
     #### write out files
-    fid.write(NewInputFile,'w')
+    fid = open(NewInputFile,'w')
     fid.writelines(lines)
     fid.close()
